7cb2caf5f7c8c80ba242c9e798d610f6fe7d4c28
yash vora <officialyashvora1978@gmail.com>
2024-03-25 21:38:29 +0530

    js course init

---
+++ script1 - variable&object.js
@@ -0,0 +1,58 @@
+// variables is container where we can store our values
+
+// Variables naming rules:
+// 1. Variable names are case sensitive
+// example: yash and Yash is different variable
+
+// 2. Variable names cannot start with a number
+// example: 789yash - show error message
+
+// 3. Letters,digits,underscore, $ is allowed only
+// example: yash-vora or yash@vora - show error message
+
+// 4. Predefined(Reserved) words are not allowed
+// example: if variable name is class - show error message
+
+// var let & const
+
+// var was used before ES6 standard but now commonly used let and const after ES6
+
+// var:
+// Variable can be re-declare & updated,
+// global scope
+
+// let:
+// Variable can't be re-declare but can be updated,
+// block scope
+
+// const:
+// Variable can't be re-declare or updated,
+// block scope
+
+
+let string = "123" + 123
+
+console.log(string);
+
+// object example 1
+const product = {
+    img : "url",
+    title : "peter parker jeans",
+    rating : 4,
+    price : 2799,
+    discount : 30,
+}
+
+console.log(product);
+
+// object example 2
+const profile = {
+    username : "Yash Vora",
+    isFollow : true,
+    posts : 125,
+    followers : 123+"k",
+    following : 123,
+    description : "Yash Vora is a backend developer"
+}
+
+console.log(profile);
